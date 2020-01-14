18020878205a393e1f51d2380425b8be3b44b639
fbahr <frederic.bahr@stud.htwk-leipzig.de>
2020-01-14 12:33:14 +0100

    Upload New File

---
+++ ConfigClient/msq_header.h
@@ -0,0 +1,10 @@
+/* msq_header.h */
+#include <stdlib.h>
+#include <signal.h>
+
+
+void checkHostName(int hostname);
+
+void checkHostEntry(struct hostent * hostentry);
+
+void checkIPbuffer(char *IPbuffer);
